11911e33d0cf50ddc3f2d918bf0ed39401f4cbaf
FM <152152436+Fallop@users.noreply.github.com>
2025-07-06 18:11:23 -0600

    CRUD v1.5

--- server.js
+++ server.js
@@ -12,15 +12,18 @@ let usuarios = [
 ];
 
 app.get('/usuarios', (req, res) => res.json(usuarios));
+
 app.get('/usuarios/:id', (req, res) => {
   const u = usuarios.find(user => user.id == req.params.id);
   u ? res.json(u) : res.status(404).send('No encontrado');
 });
+
 app.post('/usuarios', (req, res) => {
   const nuevo = { id: Date.now(), ...req.body };
   usuarios.push(nuevo);
   res.json(nuevo);
 });
+
 app.put('/usuarios/:id', (req, res) => {
   const idx = usuarios.findIndex(u => u.id == req.params.id);
   if (idx !== -1) {
@@ -30,12 +33,10 @@ app.put('/usuarios/:id', (req, res) => {
     res.status(404).send('No encontrado');
   }
 });
+
 app.delete('/usuarios/:id', (req, res) => {
   usuarios = usuarios.filter(u => u.id != req.params.id);
   res.json({ message: 'Eliminado' });
 });
 
 app.listen(port, () => console.log(`Servidor en puerto ${port}`));
-
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
